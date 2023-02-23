6705d5d65b40aca1477b4ce27f1aee10ddcc984d
Jimmy Poulsen <jimmy@poulsen.dk>
2023-02-23 20:24:50 +0100

    Rename generator and require generator in Bouncer::Engine

--- lib/bouncer/engine.rb
+++ lib/bouncer/engine.rb
@@ -1,5 +1,9 @@
 module Bouncer
   class Engine < ::Rails::Engine
     isolate_namespace Bouncer
+
+    generators do
+      require "bouncer/generators/install_generator"
+    end
   end
 end
--- lib/bouncer/railtie.rb
+++
@@ -1,7 +0,0 @@
-module Bouncer
-  class Railtie < Rails::Railtie
-    generators do
-      require "bouncer/generators/install_generator"
-    end
-  end
-end
