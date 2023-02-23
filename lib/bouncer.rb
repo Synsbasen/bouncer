0603df510eaae32911d3acec14a0ba8b2f22ee69
Jimmy Poulsen <jimmy@poulsen.dk>
2023-02-23 22:23:41 +0100

    Set required columns

--- lib/bouncer.rb
+++ lib/bouncer.rb
@@ -2,6 +2,8 @@ require "bouncer/version"
 require "bouncer/engine"
 
 module Bouncer
+  REQUIRED_COLUMNS = %w[uid provider first_name last_name email].freeze
+
   mattr_accessor :user_class,
                  :auth0_client_id,
                  :auth0_client_secret,
@@ -9,10 +11,25 @@ module Bouncer
                  :auth0_authorize_params
 
   def self.ready?
-    [user_class, auth0_client_id, auth0_client_secret, auth0_domain].all?(&:present?)
+    [user_class, auth0_client_id, auth0_client_secret, auth0_domain].all?(&:present?) &&
+      all_required_columns_present?
   end
 
   def self.user_class
     @@user_class.constantize
+  rescue NoMethodError => e
+    raise "Bouncer.user_class is not set. Please set it in an initializer."
+  rescue NameError => e
+    raise "Bouncer.user_class is set to an invalid class name. Please set it to a valid class name"
+  end
+
+  private
+
+  def self.all_required_columns_present?
+    if missing_columns = REQUIRED_COLUMNS.difference(user_class.column_names) > 0
+      raise "Bouncer is missing the following columns: #{missing_columns.join(", ")}"
+    else
+      true
+    end
   end
 end
