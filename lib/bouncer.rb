373a8492094375e7ffa6b428f73bb9698a2beb74
Jimmy Poulsen <jimmy@poulsen.dk>
2023-02-23 20:33:04 +0100

    Auth0Controller and Authentication module

--- lib/bouncer.rb
+++ lib/bouncer.rb
@@ -2,9 +2,17 @@ require "bouncer/version"
 require "bouncer/engine"
 
 module Bouncer
-  mattr_accessor :auth0_client_id, :auth0_client_secret, :auth0_domain, :auth0_authorize_params
+  mattr_accessor :user_class,
+                 :auth0_client_id,
+                 :auth0_client_secret,
+                 :auth0_domain,
+                 :auth0_authorize_params
 
   def self.ready?
-    [auth0_client_id, auth0_client_secret, auth0_domain].all?(&:present?)
+    [user_class, auth0_client_id, auth0_client_secret, auth0_domain].all?(&:present?)
+  end
+
+  def self.user_class
+    @@user_class.constantize
   end
 end
